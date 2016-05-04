1db949db4faf41456a41881e18bacfb15738c513
marmite <mailbox@a20.co.za>
2016-05-04 23:09:37 +0200

    replaced bit tweet file - internet dog slow

--- testdatagen/tweetgenerator.py
+++ testdatagen/tweetgenerator.py
@@ -5,7 +5,7 @@ import random
 if __name__ == '__main__':
 	names = ['Ward','Alan','Martin']
 	with open('bigtweets.txt','w+') as f:
-		for i in range(1,1000000):
+		for i in range(1,1000): #I generated a pretty huge file
 			t = rikeripsum.generate_sentence()
 			f.write("{}> {}\n".format(random.choice(names), t))
 			
